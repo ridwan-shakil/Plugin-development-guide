6f7dc85f032be8d82ba26d8d3543769344a2563e
ridwan-shakil <109476737+ridwan-shakil@users.noreply.github.com>
2023-03-07 12:51:02 +0600

    Create enqueue scripts.php

---
+++ enqueue scripts.php
@@ -0,0 +1,31 @@
+<?php
+
+admin_enqueue_scripts  // it's for adding css or js into admin pannle ( we can enque for only current screen  )
+wp_enqueue_scripts    // it's for adding css of js into frontend of website
+
+// ====================================
+// adding scripts for admin panel 
+// ====================================
+function pqrc_enque_admin_scripts($screen) {
+    if ($screen == 'options-general.php') {
+        // css
+        wp_enqueue_style('pqrc_minitoggle_css', plugin_dir_url(__FILE__) . "assets/css/minitoggle.css");
+
+
+        // js 
+        wp_enqueue_script('pqrc_minitoggle_js', plugin_dir_url(__FILE__) . "/assets/js/minitoggle.js", ['jquery'], '1.0', true);
+
+        wp_enqueue_script('pqrc_main_js', plugin_dir_url(__FILE__) . '/assets/js/pqrc_main.js', ['jquery'], time(), true);
+    }
+}
+add_action('admin_enqueue_scripts', 'pqrc_enque_admin_scripts');
+
+// ====================================
+// adding scripts for frontend of website
+// ====================================
+
+function posts_qrcode_enque_style() {
+    wp_enqueue_style('posts_to_qrcode_style', plugin_dir_url(__FILE__) . "style.css");
+}
+
+add_action('wp_enqueue_scripts', 'posts_qrcode_enque_style');
