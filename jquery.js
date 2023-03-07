0232d4275f3c92c2d99ce6a40cc18372a214a4f3
ridwan-shakil <109476737+ridwan-shakil@users.noreply.github.com>
2023-03-07 13:05:22 +0600

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -4,3 +4,9 @@
   
 })(jQuery);
 
+
+// If code is not working 
+    1) try by changing the code location
+    2) If code is loading from server try this methos :  $(document).on('click', '#yourid', function() { alert("hello"); });
+    3) check if the code is dependent on other code such as jQuery 
+    4)
