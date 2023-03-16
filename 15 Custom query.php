e0e324fe4c8c6666e8f883c7a8a5070204837d0f
ridwan-shakil <109476737+ridwan-shakil@users.noreply.github.com>
2023-03-16 22:12:04 +0600

    Update 15 Custom query.php

--- 15 Custom query.php
+++ 15 Custom query.php
@@ -1,6 +1,54 @@
 <?php
+ just see the wp_query documentation , It has endless possible way to show the posts .
+   
+//   main documentation :   https://developer.wordpress.org/reference/classes/wp_query/
+//   Offline documentation: https://devdocs.io/wordpress/classes/wp_query
+
 // =========================
-   Get posts by id  
+    example 
+// =========================
+$paged   = get_query_var("paged") ? get_query_var("paged") : 1;  // For pagination
+        $post_ids       = array(449, 454, 455, 456);   // Give post id 
+            'numberposts' => 10,
+            'post_type'   => 'post',
+            'post__in'    => $post_ids,   // Change this key and value to get your desired posts ( see official documentation ) 
+            'orderby'     => 'post__in',
+            'paged'       => $paged    // For pagination
+            // 'meta_key' => 'field_name',
+            // 'meta_value' => 'field_value'
+        );
+
+        $query = new WP_Query($args);
+
+        ?>
+
+        <?php if ($query->have_posts()) : ?>
+            <ul>
+                <?php while ($query->have_posts()) : $query->the_post(); ?>
+                    <li>
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </li>
+                <?php endwhile; ?>
+            </ul>
+        <?php endif; ?>
+
+        <?php wp_reset_query(); ?>
+
+
+<!-- Navigation  -->
+                 <?php
+                    echo paginate_links(array(
+                        'total' => $query->max_num_pages
+                    ));
+                 ?>
+
+
+
+
+
+<?php
+// =========================
+   Get posts by id  ( different way )
 // =========================
 
         $paged          = get_query_var("paged") ? get_query_var("paged") : 1;  // For navigation
@@ -21,7 +69,11 @@
         wp_reset_postdata();
         ?>
 
+<!-- pagination -->
+                  <?php
+                    echo paginate_links(array(
+                        'total' => ceil(count($post_ids) / $posts_per_page)
+                    ));
+                    ?>
+
 <?php
-// =========================
-   Get posts  
-// =========================
