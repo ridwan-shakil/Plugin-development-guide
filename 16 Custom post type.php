86b5f11275a0dc95fa074cb6d24193a4bd3a2c46
ridwan-shakil <109476737+ridwan-shakil@users.noreply.github.com>
2023-03-18 23:57:12 +0600

    Create

---
+++ 16 Custom post type.php
@@ -0,0 +1,22 @@
+<?php 
+
+Default post types are :
+1) Post
+2) Page
+3) Navigation menu
+4) Attachments
+5) Revisions
+
+// =============================
+  Notes
+// =============================
+1) Alloways write "CPT" inside a plugin 
+2) 
+3)
+4)
+ 
+-------------------------------
+There are 2 ways to create "CPT" 
+  1) Use " cpt ui " plugin > creat CPT > copy the code > pest inside the plugin 
+  2) Write the code from scratch 
+ 
