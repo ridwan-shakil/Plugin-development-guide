996ea184881cb41cb3474dd1cb221f1d8f90a080
ridwan-shakil <109476737+ridwan-shakil@users.noreply.github.com>
2023-03-28 17:13:03 +0600

    Create wp_options.php

---
+++ wp_options.php
@@ -0,0 +1,30 @@
+ <?php
+// wp_options table has 4 fields 
+// option_id    option_name    option_value  autoplay
+
+// we can save any value to this table & retrive the data from it 
+
+
+
+// input fields 
+<div>
+        <label for=""><?php __('Number of posts', 'dbw') ?></label>
+        <input type="number" name="dbw_numofpost" id="dbw_numofpost" value="<?php echo $num_of_post ?>">
+</div>
+ 
+
+<?php
+<!-- updation the option value into wp_option table  -->
+    
+   $num_of_post = get_option('dbw_posttoshow', 5);
+    if (isset($_POST['dbw_numofpost']) && $_POST['dbw_numofpost'] > 0) {
+        $num_of_post = sanitize_text_field($_POST['dbw_numofpost']);
+        update_option('dbw_posttoshow', $num_of_post);
+    }
+
+
+<!-- Show the option value  -->
+echo get_option('dbw_posttoshow', 5);
+
+?>
+    
