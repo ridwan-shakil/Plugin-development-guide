7cf6b254c6ec2cbe01e537e306f3c9f7dcb1e87c
ridwan-shakil <109476737+ridwan-shakil@users.noreply.github.com>
2023-06-20 00:11:19 +0600

    Create 25 Admin notice.php

---
+++ 25 Admin notice.php
@@ -0,0 +1,56 @@
+<?php
+
+// =======================================================
+//  The function displays a notice to the user
+// =======================================================
+ 
+function aicrud_admine_notice() {
+?>
+<!-- Notice information -->
+    <div id="ai-curd-notice" class="notice notice-success is-dismissible">
+        <h2>Welcome to AI Crud app :)</h2>
+        <p>You can add users name and email in this crud app and see the data in ai crud admin menu page</p>
+    </div>
+
+<?php
+}
+
+add_action('admin_notices', 'aicrud_admine_notice');
+
+// =======================================================
+// Write the notice inside this function to show the notice in only selected pages 
+// =======================================================
+ global $pagenow;
+    if (in_array($pagenow, ['index.php', 'admin.php'])) {
+     //   Notice information goes here
+};
+// =======================================================
+// if the user click on notice-dismiss button then set a cookie and don't show the notice for a time period 
+// =======================================================
+if (!isset($_COOKIE['notice_closed'])) {
+     //   Notice information goes here
+    };
+// ------------------------ jQuery code to make a cookie on a button click --------------------------
+   $('body').on('click', "#ai-curd-notice .notice-dismiss", function () {
+            createCookie('notice_closed', 'true', 20); // The cookie will expire after 20 days
+
+        });
+
+
+        // Function to create a cookie
+        function createCookie(name, value, days) {
+            var expires;
+
+            if (days) {
+                var date = new Date();
+                date.setTime(date.getTime() + (days * 1000));   // seconds 
+                // date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000)); // days
+                expires = "; expires=" + date.toGMTString();
+            } else {
+                expires = "";
+            }
+
+            document.cookie = name + "=" + value + expires + "; path=/";
+        };
+
+// ===============================================================================
