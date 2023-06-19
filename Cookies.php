dc58ebefde901aa823fa4e85c6ec3c1a0ab4872f
ridwan-shakil <109476737+ridwan-shakil@users.noreply.github.com>
2023-06-19 12:37:58 +0600

    Create Cookies.php

---
+++ Cookies.php
@@ -0,0 +1,17 @@
+<?php
+//=================================================
+// Cookie in php
+//=================================================
+
+/*Setting a cookie named "catagory" with a
+value of "Books" that will expire in 30 seconds and will be available to all paths on the website. */
+    setcookie('catagory', 'Books', time() + 30, "/");
+
+
+// Using the cookie 
+$catagory = '';
+    if (isset($_COOKIE['catagory'])) {
+        $catagory = $_COOKIE['catagory'];
+    }
+
+echo $catagory;
