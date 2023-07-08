39cb0801ccce0ea5fe9ae8c948bbf2b44527a8c2
ridwan-shakil <109476737+ridwan-shakil@users.noreply.github.com>
2023-07-08 22:00:18 +0600

    Update 07 jquery.js

--- 07 jquery.js
+++ 07 jquery.js
@@ -10,3 +10,31 @@
     2) If code is loading from server try this methos :  $(document).on('click', '#yourid', function() { alert("hello"); });
     3) check if the code is dependent on other code such as jQuery 
     4)
+
+// ==========================================
+// Ajax
+// ==========================================
+$.ajax({
+            type: "post",
+            url: "right_sidebar.php",
+            data: ',
+            success: function (response) {
+             $('.result').text(response);
+                }
+          });
+
+----------------------//Easy way of using Ajax-------------------
+//jqpost
+    $.post("url", data,
+    function (data, textStatus, jqXHR) {
+        
+    },
+    "dataType"
+);
+//jqget
+$.get("url", data,
+    function (data, textStatus, jqXHR) {
+        
+    },
+    "dataType"
+);
