4513b1f816d6f3eb561237ed78acbd0b4f1a9d2f
ridwan-shakil <109476737+ridwan-shakil@users.noreply.github.com>
2023-07-13 17:35:32 +0600

    Update Ajax request on WP.php

--- Ajax request on WP.php
+++ Ajax request on WP.php
@@ -3,7 +3,7 @@ input fields
 or
 <button class="action-btn" data-task="add-option">Add option</button>
 
-// Step ------------------------- 
+// Step 2 ------------------------- Sent ajax url & nonce to jquery file 
 <?php
 wp_enqueue_script('ajax-script', plugins_url('options_api.js', __FILE__), array('jquery'), '1.0.0', true);
 
@@ -15,6 +15,19 @@ wp_enqueue_script('ajax-script', plugins_url('options_api.js', __FILE__), array(
             'nonce'    => wp_create_nonce('title_example'),
         )
     );
-// Step -------------------------
-// Step -------------------------
+// Step 3 ------------------------- Ajax request in wordpress
+//jqajaxwp
+ $.post(my_ajax_obj.ajax_url, {      //POST request
+                _ajax_nonce: my_ajax_obj.nonce, //nonce
+                action: "rs_actions",           //action
+                data: task                      //data
+            }, function (data) {                //callback
+                $('.result').text(data);
+            }
+            );
+// Step 4 -------------------------
+add_action('wp_ajax_rs_actions', 'clbc_function_to_perform_oprations');
+// add_action('wp_ajax_{action name of ajax request }', 'functionName');
+// Here the callback function will perform oprations & handover the result to that ajax request , from wher that request came from 
+
 // Step -------------------------
