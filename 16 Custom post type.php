47285c9023d59308cf5d3dadeb5db8360bdcfc14
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2023-08-11 12:25:43 +0600

    Update 16 Custom post type.php

--- 16 Custom post type.php
+++ 16 Custom post type.php
@@ -20,3 +20,29 @@ There are 2 ways to create "CPT"
   1) Use " cpt ui " plugin > creat CPT > copy the code > pest inside the plugin 
   2) Write the code from scratch 
  
+
+
+// Change the cpt 'add title' text 
+function custom_change_title_text($title, $post) {
+    if ('team_members' === $post->post_type) {
+        $title = 'Member Name';
+    }
+    return $title;
+}
+add_filter('enter_title_here', 'custom_change_title_text', 10, 2);
+
+
+// custom function to register the "subject" taxonomy
+function team_register_taxonomy_department() {
+    register_taxonomy(
+        'department',
+        'team_members',
+        array(
+            'label' => __('Department'),
+            'rewrite' => array('slug' => 'department'),
+            'hierarchical' => true
+        )
+    );
+}
+// call our custom function with the init hook
+add_action('init', 'team_register_taxonomy_department');
