ef80b2ee870b55f2bac7d535ace10c4ab5f44f30
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2023-08-29 20:54:31 +0600

    Update #   Admin menu & submenu page.php

--- #   Admin menu & submenu page.php
+++ #   Admin menu & submenu page.php
@@ -1,9 +1,15 @@
 <?php
+// Notes:
+// 1. (Give the same slug to the top menu & first submenu ) To open the first submenu on the top menu click 
+
+
+
 add_action('admin_menu', 'register_my_custom_menu_page');
 function register_my_custom_menu_page() {
     // add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
-    add_menu_page("wpdb menu page", 'Wpdb menu', 'manage_options', 'wpdbmenu.php', 'clbc_wpdb_page', '', null);
+    add_menu_page("wpdb menu page", 'Wpdb menu', 'manage_options', 'wpdbmenu', 'clbc_wpdb_page', '', null);
 
     // add_submenu_page( $parent_slug, $page_title, $menu_title, $capability,   $menu_slug, $callback, $position );
-    add_submenu_page('wpdbmenu.php', 'wpdb insert', 'wpdb insert', 'manage_options', 'wpdb_insert.php', 'clbc_wpdb_insert');
+    add_submenu_page('wpdbmenu', 'wpdb insert', 'wpdb insert', 'manage_options', 'wpdbmenu', 'clbc_wpdb_insert');  // it has given the same slug as the top menu ( so it opens on both top menu & sub-menu click)
+    add_submenu_page('wpdbmenu', 'wpdb update', 'wpdb update', 'manage_options', 'wpdb-update', 'clbc_wpdb_update');
 }
