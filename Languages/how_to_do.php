d203f6ee82fa754c4207998b4cc720d0104c73a7
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2023-09-04 12:34:38 +0600

    Create how_to_do.php

---
+++ Languages/how_to_do.php
@@ -0,0 +1,15 @@
+Your work is only to create a .pot file 
+
+There are 3 type of files 
+one .pot file 
+one .po file for every language 
+one .mo file for every language 
+
+There are wordpress won team to translate the theme & plugins , they are called translators . 
+Those Translators work is to crate .po & .mo files for every languages 
+
+there are many ways to crate an .pot file   |  see the documentation https://developer.wordpress.org/plugins/internationalization/localization/
+You can use poedit software to create this 3 types of files 
+
+[ After generating the PO file you can rename the file to POT. If a MO was generated then you can delete that file as it is not needed.
+If you don’t have the pro version you can easily get the Blank POT and use that as the base of your POT file. ]
