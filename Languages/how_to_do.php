00352dddf747c608da564021dad415560e40e9a1
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2023-09-04 21:20:13 +0600

    Update how_to_do.php

--- Languages/how_to_do.php
+++ Languages/how_to_do.php
@@ -11,5 +11,12 @@ Those Translators work is to crate .po & .mo files for every languages
 there are many ways to crate an .pot file   |  see the documentation https://developer.wordpress.org/plugins/internationalization/localization/
 You can use poedit software to create this 3 types of files 
 
+-------------------------
+Create .pot file 
+-------------------------
+open Poedit software 
+File --> New from pot/po files --> select  "bulk-wordrpess " file --> click "Extract from source --> click + & add the plugin main folder -->
+add functions that you used to translate --> generate .po & .mo file -- Save files
+
 [ After generating the PO file you can rename the file to POT. If a MO was generated then you can delete that file as it is not needed.
 If you don’t have the pro version you can easily get the Blank POT and use that as the base of your POT file. ]
