b9096c57f37ec19e3ef65878d7a4193331a3fe70
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2023-09-11 20:52:09 +0600

    Create uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,25 @@
+<?php
+When the post and page is permanently deleted, everything that is tied to it is deleted also. 
+This includes comments, post meta fields, and terms associated with the post.
+
+The post or page is moved to Trash instead of permanently deleted unless Trash is disabled, 
+item is already in the Trash, or $force_delete is true.
+
+
+
+
+// If uninstall not called from WordPress, then exit.
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+	exit;
+}
+  
+// ==========================
+//  Clear database 
+// ==========================
+  
+  // Clear CPT data 
+  $teams = get_posts(array('post_type' => 'team_member_showcase', 'numberposts' => -1));
+  foreach ($teams as $team) {
+  	wp_delete_post($team->ID, true);
+  }
+
