4b8b93f0c37b1131d0d54b575178767f586b4f1c
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2023-09-11 21:35:20 +0600

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,4 @@
 <?php
-When the post and page is permanently deleted, everything that is tied to it is deleted also. 
-This includes comments, post meta fields, and terms associated with the post.
-
-The post or page is moved to Trash instead of permanently deleted unless Trash is disabled, 
-item is already in the Trash, or $force_delete is true.
-
-
-
 
 // If uninstall not called from WordPress, then exit.
 if (!defined('WP_UNINSTALL_PLUGIN')) {
@@ -17,13 +9,20 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 //  Clear database 
 // ==========================
   
- // Clear database 
+ // Clear CPT data
 $teams = get_posts(array(
 	'post_type' => 'team_member_showcase',
 	'numberposts' => -1,
 	'post_status' => array('publish', 'draft', 'auto-draft', 'trash'),
 ));
 foreach ($teams as $team) {
-	wp_delete_post($team->ID, true);
+	wp_delete_post($team->ID, true);  // everything that is tied to the CPT is deleted also. This includes comments, post meta fields, and terms associated with the post.
 }
 
+// alternative way of clearing CPT data  -> Access the database via SQL
+global $wpdb;
+$wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'team_member_showcase'" );
+$wpdb->query( "DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)" );
+$wpdb->query( "DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT id FROM wp_posts)" );
+
+// -------------------------
