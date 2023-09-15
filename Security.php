45eeb7d5af35d7e27d5914c2671a6d7d8b6f515b
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2023-09-15 19:42:51 +0600

    Update Security.php

--- Security.php
+++ Security.php
@@ -1,5 +1,11 @@
-
+// =============================
+// Methods to protect data & requests
+// =============================
 
 Securing INPUT  -> Sanitization , Validation
 
 Securing OUTPUT -> escaping 
+
+USER ROLE / CAPABILITIES
+
+NONCES
