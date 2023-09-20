2c1f73958dde1c1819da3a6d5fa956da7cfe1595
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2023-09-20 21:49:46 +0600

    Update Caching.php

--- Caching.php
+++ Caching.php
@@ -1,6 +1,10 @@
 <?php
 // wp object cache 
-
+  // Non-persistent
+        by default, it is enabled 
+          
+  // Persistent 
+        we need to download any persistent caching plugin like: Memcache, reis, etc
 
 
 // Transient api  
