3ab63eb3c6000fe14fdf23c9f58f365686342850
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2023-10-04 21:16:09 +0600

    Create Authentication.php

---
+++ Rest API/Authentication.php
@@ -0,0 +1,11 @@
+there are sevarel plugin for creating api authentication 
+1) JWT Authentication for WP-API
+2) enter only user name & password in postman [ just for practice ]
+
+-------------------------------
+JWT Authentication for WP-API
+-------------------------------
+create a POST request in this endpoint  :     /wp-json/jwt-auth/v1/token 
+( send tis two paramiter with the request :   'username' = ridwan  , 'password' = ridwan )
+
+/wp-json/jwt-auth/v1/token/validate | POST
