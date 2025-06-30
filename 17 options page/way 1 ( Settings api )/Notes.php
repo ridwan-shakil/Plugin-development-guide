d834e2b00ae6cd3b7aa888e131faeb90ecff91c9
MD.Ridwan <109476737+ridwan-shakil@users.noreply.github.com>
2025-06-30 13:36:43 +0600

    Create Notes.php

---
+++ 17 options page/way 1 ( Settings api )/Notes.php
@@ -0,0 +1,13 @@
+2 ways to create option page : Settings api / Direct form
+
+(Way 1) Settings api :
+    1) Genrate code from ( hasty or genratewp )
+    2) after generating the options page settings :
+      * Make the labels translatable 
+      * Prefix the ID/Class with "text domain" to avoid conflicts
+      * Test if all the fields are saving data properly or not 
+      *
+
+
+================= OR ==================
+Dirrect form creat & handle manually
